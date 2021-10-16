b6af135fc644f36120e72263b0b763a1eaba086f
Shrut26 <88401620+Shrut26@users.noreply.github.com>
2021-10-17 01:28:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if(isset($_POST['name'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcomoe to Travel plan </title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/firstbackend/style.css">
     
 </head>
 
